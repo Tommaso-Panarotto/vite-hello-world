dc3988cf0d1fb05c6a3dcdc661e0e18ff1449dd1
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-13 16:58:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<style scoped></style>
